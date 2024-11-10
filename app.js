c1b4a4c621268fe3d8dbb5a185952b1b546ba7a8
Abdula Ahmed <ahmed.abdula.hslc@gmail.com>
2024-11-09 19:37:49 -0500

    Cache Elements

---
+++ app.js
@@ -0,0 +1,8 @@
+// Cache elements
+const randomQuoteBtn = document.getElementById("random-quote-btn");
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const quoteForm = document.getElementById("quote-form");
+const quoteInput = document.getElementById("quote-input");
+const errorMessage = document.getElementById("error-message");
